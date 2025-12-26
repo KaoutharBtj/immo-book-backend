229e01e26d6883010fa012900bf25ed0779503df
kaoutharboutjanouite <boutjanouitek@gmail.com>
2025-12-26 15:51:52 +0100

    "Change projectsRoute name"

--- routes/promoProjectsRoutes.js
+++ routes/promoProjectsRoutes.js
@@ -16,14 +16,14 @@ const {createProject,
 
 router.get('/search', searchProject);
 router.get('/mes-projets', protect, authorize('promoteur'), getMyProject);
-router.get('/:id', protect, authorize('promoteur'), getProjectById);
+router.get('/mes-projets/:id', protect, authorize('promoteur'), getProjectById);
 
-router.post('/', protect, authorize('promoteur'), createProject);
-router.put('/:id', protect, authorize('promoteur'), updateProject);
-router.delete('/:id', protect, authorize('promoteur'), deleteProject);
+router.post('/mes-projets', protect, authorize('promoteur'), createProject);
+router.put('/mes-projets/:id', protect, authorize('promoteur'), updateProject);
+router.delete('/mes-projets/:id', protect, authorize('promoteur'), deleteProject);
 
-router.post('/:id/phases', protect, authorize('promoteur'), addPhase);
-router.put('/:id/phases/:phaseId', protect, authorize('promoteur'), updatePhase);
-router.delete('/:id/phases/:phaseId', protect, authorize('promoteur'), deletePhase);
+router.post('/mes-projets/:id/phases', protect, authorize('promoteur'), addPhase);
+router.put('/mes-projets/:id/phases/:phaseId', protect, authorize('promoteur'), updatePhase);
+router.delete('/mes-projets/:id/phases/:phaseId', protect, authorize('promoteur'), deletePhase);
 
 module.exports = router;
